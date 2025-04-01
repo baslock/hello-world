32c7c007ac3a3dcc45f59c62ee5959c2fbf887ed
Will B <baslock@gmail.com>
2025-04-01 16:30:30 -0400

    adding powershell to the repo

---
+++ hello_shworld.ps1
@@ -0,0 +1,8 @@
+# Output a greeting message
+Write-Host "Hello World!" -ForegroundColor Green
+
+# Output a farewell message
+Write-Host "Goodbye Universe!" -ForegroundColor Cyan
+
+# Output an additional message
+Write-Host "Committed this change from Windows" -ForegroundColor Yellow
