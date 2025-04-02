814a4e1e4a62a030baf1747826071e613ee287ed
Will B <baslock@gmail.com>
2025-04-02 19:31:22 -0400

    adding get-process plus pretty options

--- hello_shworld.ps1
+++ hello_shworld.ps1
@@ -6,3 +6,5 @@ Write-Host "Goodbye Universe!" -ForegroundColor Cyan
 
 # Output an additional message
 Write-Host "Committed this change from Windows" -ForegroundColor Yellow
+
+Get-Process | Sort-Object CPU -Descending | Select-Object -First 5 | Format-Table -AutoSize
